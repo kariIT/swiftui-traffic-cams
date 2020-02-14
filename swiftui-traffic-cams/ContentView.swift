fdb017c664c4693912b0b39b12c4b2158aa4b948
kariIT <joona.heinikari@elisanet.fi>
2020-02-14 12:29:32 +0200

    initial commit

---
+++ swiftui-traffic-cams/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-traffic-cams
+//
+//  Created by Joona Heinikari on 14/02/2020.
+//  Copyright © 2020 Joona Heinikari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
